9fc8b48c2c58335e1eec52e71fcff605680eb2ba
harryallenp1 <122371071+harryallenp1@users.noreply.github.com>
2023-02-02 09:13:34 -0500

    Delete Week 2 directory

--- Week 2/HelloModule.py
+++
@@ -1,8 +0,0 @@
-class CourseOrientation:
-        def displayCourseInfo(self):
-            courseTitle = "Programming Principles"
-            courseCode = 10004
-
-            print(courseCode, courseTitle)
-
-print("THe name of module witin the function", __name__)
