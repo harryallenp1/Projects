d55a3d682c28aa21a18ed44f65732c978e88928f
harryallenp1 <122371071+harryallenp1@users.noreply.github.com>
2023-02-02 09:41:05 -0500

    Changes are Updated

---
+++ Week 1/HelloPythonv2.py
@@ -0,0 +1,12 @@
+#Imperative Programming Paradigm
+print("Good Morning, JK")
+
+#creating a variable to store information
+#naming convention for variables is CamelCase
+greetingMessage = "Good Morning Everyone"
+courseTitle = "Programming Principles"
+courseCode = 10004
+
+print(greetingMessage)
+print(greetingMessage, courseCode, courseTitle, sep="--")
+print("PROG", courseCode, sep="")
---
+++ Week 1/HelloPythonv3.py
@@ -0,0 +1,20 @@
+#Functional Programming Paradigm
+print("Trying to implement functional programming paradigm")
+
+def displayCourseInfo():
+    courseTitle = "Programming Principles"
+    courseCode = 10004
+
+    print(courseCode, courseTitle)
+
+print("Calling function")
+displayCourseInfo()
+
+print("Showing information for student 1")
+displayCourseInfo()
+
+print("Showing information for student 2")
+displayCourseInfo()
+
+print("Showing information for student 3")
+displayCourseInfo()
